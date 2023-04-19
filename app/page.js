fef47b3671b1ddda07442efe1313c3ce0aa283a7
Caroline Leeper <caroline.leeper@gmail.com>
2023-04-20 00:39:52 +1000

    connect to supabase

--- app/page.js
+++ app/page.js
@@ -1,10 +1,30 @@
+"use client";
+
 import Header from "./components/Header";
 import WeekCarousel from "./components/WeekCarousel";
 import ActionButtons from "./components/ActionButtons";
-
-//work out today's date - request 28 days from today with correspoinding recipes from server
+import supabase from "./utils/supabase";
 
 export default async function Home() {
+	const date = new Date();
+
+	const now = date
+		.toLocaleDateString("en-AU", { timeZone: "Australia/Melbourne" })
+		.split("/")
+		.reverse()
+		.join("-");
+
+	date.setDate(date.getDate() + 28);
+
+	const later = date
+		.toLocaleDateString("en-AU", { timeZone: "Australia/Melbourne" })
+		.split("/")
+		.reverse()
+		.join("-");
+
+	const { data: days } = await supabase.from("days").select().gte("date", now).lt("date", later);
+	const { data: recipes } = await supabase.functions.invoke("get-all-recipes");
+
 	return (
 		<>
 			<Header />
