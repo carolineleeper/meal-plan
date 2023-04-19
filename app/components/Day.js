ce3984950f81104296218057cf509ffe183112a1
Caroline Leeper <caroline.leeper@gmail.com>
2023-04-19 23:10:09 +1000

    create day component

---
+++ app/components/Day.js
@@ -0,0 +1,15 @@
+const Day = () => {
+	return (
+		<>
+			<h2>Today</h2>
+			<h3>19/4/23</h3>
+			<p>Roast Chicken</p>
+			<button>Shuffle Recipe</button>
+			<button>Not cooking today</button>
+			<input type="checkbox" id="shoppingList" name="shoppingList" value="onShoppingList" />
+			<label for="shoppingList">Add to shopping list</label>
+		</>
+	);
+};
+
+export default Day;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import recipes from "./recipes.json";
+import Day from "./components/day";
 
 const getRecipe = () => {
 	const recipe = recipes[Math.floor(Math.random() * recipes.length)];
@@ -8,11 +9,14 @@ const getRecipe = () => {
 	return recipe;
 };
 
+//work out today's date - request 28 days from today with correspoinding recipes from server
+
 export default async function Home() {
 	return (
 		<>
 			<h1 className="text-5xl text-emerald-600">Meal Plan</h1>
 			<button onClick={getRecipe}>Get Recipe</button>
+			<Day />
 		</>
 	);
 }
